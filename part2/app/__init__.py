781852577c82254f3242c2f5972675f6aefdbe89
Alexpena76 <alexpenasilva@gmail.com>
2025-10-22 11:23:54 -0500

    Q3

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -6,6 +6,7 @@ Creates and configures the Flask application instance
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
 
 def create_app():
     """
@@ -25,6 +26,7 @@ def create_app():
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
     
     # Future namespaces will be added here:
     # api.add_namespace(places_ns, path='/api/v1/places')
